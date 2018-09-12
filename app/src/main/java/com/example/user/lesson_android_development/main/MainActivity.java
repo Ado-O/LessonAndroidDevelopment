f1c55d68ecc767a5192b27c77c0a7a72b360827c
ado-O <osijan.admir@gmail.com>
2018-09-12 10:45:33 +0200

    add image slide with dots, and manual or autoSlide

--- app/src/main/java/com/example/user/lesson_android_development/main/MainActivity.java
+++ app/src/main/java/com/example/user/lesson_android_development/main/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.user.lesson_android_development.main;
 
-import android.databinding.DataBindingUtil;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
 import com.example.user.lesson_android_development.R;
 
-import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    ViewPager viewPager;
+    int images[] = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_2};
+    ViewPagerAdapter myCustomPagerAdapter;
+    TabLayout indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +28,36 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbar = findViewById(R.id.toolbar);
 
-        setupToolbar();
-    }
 
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        indicator=(TabLayout)findViewById(R.id.indicator);
+
+        myCustomPagerAdapter = new ViewPagerAdapter(MainActivity.this, images);
+        viewPager.setAdapter(myCustomPagerAdapter);
+        indicator.setupWithViewPager(viewPager, true);
+
+        /**
+         * autorefresh
+         */
+//        Timer timer = new Timer();
+//        timer.scheduleAtFixedRate(new SliderTimer(), 4000, 6000);
+    }
+//    private class SliderTimer extends TimerTask {
+//
+//        @Override
+//        public void run() {
+//            MainActivity.this.runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    if (viewPager.getCurrentItem() < images.length - 1) {
+//                        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+//                    } else {
+//                        viewPager.setCurrentItem(0);
+//                    }
+//                }
+//            });
+//        }
+//    }
     /**
      * Setting up the toolbar, toolbar actions & title
      */
@@ -46,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+
 }
 
 
---
+++ app/src/main/java/com/example/user/lesson_android_development/main/ViewPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.user.lesson_android_development.main;
+
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.user.lesson_android_development.R;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends PagerAdapter {
+    Context context;
+    int images[];
+    LayoutInflater layoutInflater;
+
+
+    public ViewPagerAdapter(Context context, int images[]) {
+        this.context = context;
+        this.images = images;
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+        View itemView = layoutInflater.inflate(R.layout.slidingimages_layout, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        container.addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
